1b044f0b559c12ea1381a97dab25564753c0c30c
Janybaeva Saikal <zanybaevasajkal@gmail.com>
2023-02-14 21:48:56 +0600

    Completed the first task

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 //1. Напишите функцию, которая выводит ваше имя и фамилию.
 // functionName("Erjan", "Kydyrov") // My  name is Erjan Kydyrov
 let example = {};
+let myname = { 
+  name: "Saikal",
+
+  myName: function() {
+    console.log("hello, I'm " + this.name);
+  }
+};
+console.log(myname.name);
